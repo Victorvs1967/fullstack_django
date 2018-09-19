1a129d5ecfa3bf33c6c3c3f37fa6145013e4a3e8
Victorvs1967 <victorsmirnov@me.com>
2018-09-19 17:01:53 +0300

    First commit project

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    path('post/<pk>/', views.post_detail, name='post_detail'),
 ]
