fa1e1f804598720eacd51f580f3bbcbc8a94f973
Victorvs1967 <victorsmirnov@me.com>
2018-09-20 15:34:55 +0300

    First commit project

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,13 @@
+from django.conf.urls import url
 from django.urls import path
 from . import views
 
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
-    path('post/(<pk>/edit/', views.post_edit, name='post_edit'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
     path('post/new/', views.post_new, name='post_new'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -12,6 +12,18 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
+def post_draft_list(request):
+    """"""
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
@@ -23,7 +35,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
