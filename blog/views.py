22f4d7acbc1fbfc900330151c51e4bc5a7d2f737
Victorvs1967 <victorsmirnov@me.com>
2018-09-20 23:06:46 +0300

    First commit project

--- blog/views.py
+++ blog/views.py
@@ -10,11 +10,13 @@ from .models import Post
 from .forms import PostForm
 
 # Create your views here.
+@login_required
 def post_list(request):
     """"""
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    # posts = Post.objects.filter(author=request.user).order_by('published_date')
+    # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(author=request.user).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
 
 @login_required
 def post_draft_list(request):
@@ -71,6 +73,7 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
 
 def register(request):
     """"""
--- fullstack/settings.py
+++ fullstack/settings.py
@@ -138,6 +138,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# LOGIN_REDIRECT_URL = '/'
-# LOGIN_URL = 'accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = 'accounts/login/'
 
