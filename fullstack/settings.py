e2525f5097b8ece13dbed9bbda31f441f477abfc
Victorvs1967 <victorsmirnov@me.com>
2018-09-22 16:57:14 +0300

    First commit project

--- fullstack/settings.py
+++ fullstack/settings.py
@@ -95,7 +95,7 @@ else:
     }
 
     # SECURITY WARNING: don't run with debug turned on in production!
-    DEBUG = True
+    DEBUG = False
     ALLOWED_HOSTS = ['127.0.0.1', 'victors.pythonanywhere.com']
 
 
