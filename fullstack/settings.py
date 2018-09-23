37d836e9f65e3c668652e1470db3d23d8980b129
Victorvs1967 <victorsmirnov@me.com>
2018-09-23 20:42:48 +0300

    First commit project

--- fullstack/settings.py
+++ fullstack/settings.py
@@ -116,6 +116,9 @@ DATABASES = {
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'staticfiles'),
+)
 
 if os.getcwd() == '/app':
     import dj_database_url
