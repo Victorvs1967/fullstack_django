da4d8b67e963c960459beaebc9605cf9ee0efb4b
Victorvs1967 <victorsmirnov@me.com>
2018-09-23 11:43:10 +0300

    First commit project

--- fullstack/settings.py
+++ fullstack/settings.py
@@ -84,7 +84,7 @@ if os.getcwd() == '/app':
     django_heroku.settings(locals())
 
     # SECURITY WARNING: don't run with debug turned on in production!
-    DEBUG = False
+    DEBUG = True
     ALLOWED_HOSTS = ['fullstack-django.herokuapp.com']
 
     STATIC_URL = '/static/'
