5c970bbc2b6ea5dde2a473fac03a7a057e08c3fe
Victorvs1967 <victorsmirnov@me.com>
2018-09-23 20:55:12 +0300

    First commit project

--- fullstack/settings.py
+++ fullstack/settings.py
@@ -114,18 +114,15 @@ DATABASES = {
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 if os.getcwd() == '/app':
     import dj_database_url
     import django_heroku
 
     # SECURITY WARNING: don't run with debug turned on in production!
-    DEBUG = False
+    DEBUG = True
     ALLOWED_HOSTS = ['fullstack-django.herokuapp.com']
 
     DATABASES = {
