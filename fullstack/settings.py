f1f991f9b7974e6fd2644c91578f0d4e7202d84c
Victorvs1967 <victorsmirnov@me.com>
2018-09-23 14:23:52 +0300

    First commit project

--- fullstack/settings.py
+++ fullstack/settings.py
@@ -72,7 +72,7 @@ WSGI_APPLICATION = 'fullstack.wsgi.application'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'accounts/login/'
