e57c3e5134d555bf1388676da4164f8fb2b9d49a
Victorvs1967 <victorsmirnov@me.com>
2018-09-23 17:11:19 +0300

    First commit project

--- fullstack/settings.py
+++ fullstack/settings.py
@@ -123,6 +123,7 @@ if os.getcwd() == '/app':
     import django_heroku
 
     # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = False
     ALLOWED_HOSTS = ['fullstack-django.herokuapp.com']
 
     DATABASES = {
