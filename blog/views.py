fd5f0bfbfb38555ad95037fe19ed89ce251bc63c
Victorvs1967 <victorsmirnov@me.com>
2018-09-24 18:00:16 +0300

    First commit project

--- blog/views.py
+++ blog/views.py
@@ -54,7 +54,7 @@ def post_new(request):
             post = form.save(commit=False)
             post.author = request.user
             post.save()
-            return redirect('post_detail', pk=post.pk)
+            return redirect('blog:post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
