b00d11b5e5cf6cf2a10630c6c6a9461c0d347c40
Victorvs1967 <victorsmirnov@me.com>
2018-09-26 12:33:02 +0300

    First commit project

--- blog/views.py
+++ blog/views.py
@@ -70,7 +70,6 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('blog:post_detail', pk=post.pk)
     else:
