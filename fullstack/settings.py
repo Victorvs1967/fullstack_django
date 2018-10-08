96d135c3b5ee4391dd4bf5328fcb0ca981fbb8cb
Victorvs1967 <victorsmirnov@me.com>
2018-10-08 12:03:06 +0300

    First commit project

--- fullstack/settings.py
+++ fullstack/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'fullstack.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'fullstack/templates')],
+        'DIRS': ['./templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
